67e8da6f6a48197946b3ee127cabe111e004838e
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-06 19:39:11 +0200

    configure apollo client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import './styles/index.css';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
 
-root.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
