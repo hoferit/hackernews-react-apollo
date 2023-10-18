6914e54eb8ed4eade6b35b9c3291f9f55099b215
Michael Hofer <hoferdev.mi@gmail.com>
2023-10-18 10:41:32 +0200

    create Search.js component

---
+++ src/components/Search.js
@@ -0,0 +1,46 @@
+import { gql, useLazyQuery } from '@apollo/client';
+import React, { useState } from 'react';
+import Link from './Link';
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      id
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Search = () => {
+  const [searchFilter, setSearchFilter] = useState('');
+  return (
+    <>
+      <div>
+        Search
+        <input type="text" onChange={(e) => setSearchFilter(e.target.value)} />
+        <button>OK</button>
+      </div>
+      {data &&
+        data.feed.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+        ))}
+    </>
+  );
+};
+
+export default Search;
